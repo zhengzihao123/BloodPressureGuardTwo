4c70cdbc46defe8cbf6e71a4239702d9167e8d36
zhengzihao123 <1162946609@qq.com>
2017-06-09 11:33:06 +0800

    Initial commit

---
+++ app/src/main/java/test/jiyun/com/bloodpressureguard/MainActivity.java
@@ -0,0 +1,13 @@
+package test.jiyun.com.bloodpressureguard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
