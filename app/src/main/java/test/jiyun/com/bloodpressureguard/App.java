a603c93d0002d2cb0186c0fd05744a4b502a00b1
15513711739 <1257131879@qq.com>
2017-06-09 15:19:01 +0800

    基类的抽取  继承类 韩志军

--- app/src/main/java/test/jiyun/com/bloodpressureguard/App.java
+++ app/src/main/java/test/jiyun/com/bloodpressureguard/App.java
@@ -1,11 +1,13 @@
 package test.jiyun.com.bloodpressureguard;
 
+import android.app.Application;
+
 import test.jiyun.com.bloodpressureguard.base.BaseActivity;
 
 /**
  * Created by 韩志军 on 2017/6/9.
  */
 
-public class App {
+public class App extends Application{
     public static BaseActivity baseActivity;
 }
