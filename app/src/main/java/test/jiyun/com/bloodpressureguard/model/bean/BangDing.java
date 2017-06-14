12122db37172de3c3f2eca939999b40f02bb88e3
liuzhenxin2156 <564546277@qq.com>
2017-06-14 08:12:17 +0800

    1

---
+++ app/src/main/java/test/jiyun/com/bloodpressureguard/model/bean/BangDing.java
@@ -0,0 +1,39 @@
+package test.jiyun.com.bloodpressureguard.model.bean;
+
+/**
+ * 项目名称：城市通
+ * 类描述：
+ * 创建人 londn
+ * 创建时间： 2017-6-13
+ * 修改人：
+ * 修改时间：
+ * 修改内容：
+ */
+
+public class BangDing {
+
+
+    /**
+     * code : 10000
+     * error :
+     */
+
+    private int code;
+    private String error;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
